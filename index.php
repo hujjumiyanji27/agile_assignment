3f6391ebc7e74eb25250d2c562751fe55ebc21ac
hujaifa miyanji <huzefzadaa410@gmail.com>
2023-11-05 21:29:33 +0000

    https://github.com/hujaifa410/agile.git

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Alumni+Sans+Collegiate+One&family=Roboto+Slab:wght@100&display=swap" rel="stylesheet">
+
+</head>
+<header>  <!-- for header -->
+    <?php
+    include_once 'header.php';
+    ?></header>
+<body>
+
+    <div class="box">
+        <img src="shape_texture_banner.png" alt="shape image...">
+    </div>
+
+    <!-- about us card-->
+    <div class="card" style="width: 18rem; margin: 20px 20px;"> 
+  <img src="logo.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <!-- <h5 class="card-title">About Us</h5> -->
+    <a href="#" class="btn btn-primary">About Us</a>
+    <p class="card-text">Never heard of us before? Click here to find out more about our organisation</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+</div>
+
+<!-- ChiCI’s Ethics -->
+<div class="card" style="width: 18rem; margin: 20px 30%;"> 
+  <img src="ethics_word_bubble.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <!-- <h5 class="card-title">About Us</h5> -->
+    <a href="#" class="btn btn-primary">ChiCI’s Ethics</a>
+    <p class="card-text">We’ve participated in a variety of conferences and events that discuss the ethical implications of children’s participation in research.</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+</div>
+
+<!-- Collaborations -->
+<div class="card" style="width: 18rem; margin: 20px 60%;"> 
+  <img src="collab_thumb.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <!-- <h5 class="card-title">About Us</h5> -->
+    <a href="#" class="btn btn-primary">Collaborations</a>
+    <p class="card-text">Over the last 20 years, we’ve worked with many schools, universities and organisations.</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+</div>
+
+
+<!-- second container start here -->
+<!-- first one here -->
+
+    <div class="container2">
+        <h1>How we Improve Technology for Children</h1>
+        <img decoding="async" src="https://chici.org/wp-content/uploads/2021/11/point_1.png" alt="" class="wp-image-482" width="180" height="180" srcset="https://chici.org/wp-content/uploads/2021/11/point_1.png 360w, https://chici.org/wp-content/uploads/2021/11/point_1-300x300.png 300w, https://chici.org/wp-content/uploads/2021/11/point_1-150x150.png 150w" sizes="(max-width: 180px) 100vw, 180px">
+        <p>Design Work</p>
+    </div>
+
+    <!-- second one -->
+    <div class="container2">
+        <img decoding="async" loading="lazy" src="https://chici.org/wp-content/uploads/2021/11/point_2.png" alt="" class="wp-image-483" width="186" height="185" srcset="https://chici.org/wp-content/uploads/2021/11/point_2.png 371w, https://chici.org/wp-content/uploads/2021/11/point_2-300x300.png 300w, https://chici.org/wp-content/uploads/2021/11/point_2-150x150.png 150w" sizes="(max-width: 186px) 100vw, 186px">
+        <p>Evaluation of Technology</p>
+    </div>
+
+    <!-- third one -->
+
+    <div class="container2">
+    <img decoding="async" loading="lazy" src="https://chici.org/wp-content/uploads/2021/11/point_3.png" alt="" class="wp-image-484" width="180" height="180" srcset="https://chici.org/wp-content/uploads/2021/11/point_3.png 360w, https://chici.org/wp-content/uploads/2021/11/point_3-300x300.png 300w, https://chici.org/wp-content/uploads/2021/11/point_3-150x150.png 150w" sizes="(max-width: 180px) 100vw, 180px">
+        <p>Improving Research Practicesy</p>
+    </div>
+
+    <!-- fourth one -->
+    <div class="container2">
+    <img decoding="async" loading="lazy" src="https://chici.org/wp-content/uploads/2021/11/point_4.png" alt="" class="wp-image-485" width="180" height="180" srcset="https://chici.org/wp-content/uploads/2021/11/point_4.png 360w, https://chici.org/wp-content/uploads/2021/11/point_4-300x300.png 300w, https://chici.org/wp-content/uploads/2021/11/point_4-150x150.png 150w" sizes="(max-width: 180px) 100vw, 180px">
+        <p>Consultancy</p>
+    </div>
+</div>
+</body>
+</html>
